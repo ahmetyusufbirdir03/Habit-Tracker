f3b47f34a01eb48b276b36734caf4c97218621f7
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2025-10-24 10:57:01 +0300

    Weekly Schedulers CRUD operations added

---
+++ Contracts/Habit.Tracker.Contracts/Dtos/WeeklyScheduler/Update/UpdateWeeklySchedulerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Habit.Tracker.Contracts.Dtos.WeeklyScheduler.Update;
+
+public class UpdateWeeklySchedulerDto
+{
+    public Guid Id { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+    public TimeSpan ReminderTime { get; set; }
+}
--- Contracts/Habit.Tracker.Contracts/Dtos/WeeklyScheduler/WeeklySchedulerResponseDto.cs
+++ Contracts/Habit.Tracker.Contracts/Dtos/WeeklyScheduler/WeeklySchedulerResponseDto.cs
@@ -2,4 +2,9 @@
 
 public class WeeklySchedulerResponseDto
 {
+    public Guid Id { get; set; }
+    public Guid HabitId { get; set; }
+    public TimeSpan ReminderTime { get; set; }
+
+    public DayOfWeek DayOfWeek { get; set; }
 }
